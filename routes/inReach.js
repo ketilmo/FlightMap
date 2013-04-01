e7bd860a92b88ba2b3810687444d582054bc749c
Ketil Moland Olsen <ketil@vimond.com>
2013-04-01 22:52:19 +0200

    Improved async design

--- routes/inReach.js
+++ routes/inReach.js
@@ -1,4 +1,3 @@
-// Setup database schemas
 var mongooseModels = require('../lib/mongooseModels'),
 	globalEvents = require('../lib/globalEvents'),
 	GeoJSON = require('geojson'),
@@ -7,84 +6,107 @@ var mongooseModels = require('../lib/mongooseModels'),
 
 exports.postInReachEntries = function(req, res){
 	
-	saveInReachEntries(req.body, function(message, statusCode){
-		res.setHeader('Content-Type', 'application/json; charset=utf-8');
-		res.send(message, statusCode);
-		res.end;
-	});
-	
-};
+	// Validate posted data. If it is ok, commit it to the database.
+	validateInReachData(req.body, commitInReachData);
 
-function saveInReachEntries(inReachEntries, callback){
-
-	try {
+	function validateInReachData(inReachEntries, commitInReachData)
+	{
+		try {
 		
-		// Validate payload
-		check((typeof inReachEntries.Events === "undefined"), 'No Events found in payload.').equals(false);
-	}
-	catch (e) {
-			
-		// Paylod is missing Events.
-		callback('{ "Error": "' + e.message + '"}', 400);
-		return;
-	}
-
-	var trackPoints = [];
-	for (var i=0; i < inReachEntries.Events.length; i++){
+			// Check that payload contains at least one event.
+			check((typeof inReachEntries.Events === "undefined"), 'No Events found in payload.').equals(false);
+		}
 		
-		var inReachEntry = inReachEntries.Events[i];
-
-		// Is the transmitted data valid?
-		try {		
-			check(parseInt(inReachEntry.imei), 'Missing or invalid imei.').notNull().isNumeric().len(15,15);
-			check(parseInt(inReachEntry.point.latitude), 'Missing or invalid latitude.').isNumeric();
-			check(parseInt(inReachEntry.point.longitude), 'Missing or invalid longitude.').isNumeric();
-			check(parseInt(inReachEntry.point.altitude), 'Missing or invalid altitude.').isNumeric();
-			check(parseInt(inReachEntry.point.course), 'Missing or invalid course.').isNumeric();
-			check(parseInt(inReachEntry.point.speed), 'Missing or invalid speed.').isNumeric();
-			check(parseInt(inReachEntry.messageCode), 'Missing or invalid messageCode.').isNumeric();
-			check(moment(inReachEntry.timeStamp).isValid(), 'Missing or invalid timeStamp.').equals(true);    
-		} 
-
 		catch (e) {
-			// Invalid data submitted.
-			callback('{ "Error": "' + e.message + '"}', 400);
+			
+			// If payload is missing events, return error response.
+			sendResponse('{ "Error": "' + e.message + '"}', 400);
 			return;
 		}
 
-		// Data is valid.
-		var trackPoint = new mongooseModels.TrackPoint({ 
-			trackerId: inReachEntry.imei,
-			messageCode: inReachEntry.messageCode,
-			message: inReachEntry.freeText,
-			timeStamp: inReachEntry.timeStamp,
-			location: { longitude: inReachEntry.point.longitude, latitude: inReachEntry.point.latitude },
-			altitude: inReachEntry.point.altitude,
-			course: inReachEntry.point.course,
-			speed: inReachEntry.point.speed
-		}); 
+		// Create an empty array to hold the trackPoints.
+		var trackPoints = [];
 
-		trackPoints.push(trackPoint);
-	}
+		for (var i=0; i < inReachEntries.Events.length; i++){
+		
+			// Create shorthand variable for inReachEntries.Events[i].
+			var inReachEntry = inReachEntries.Events[i];
 
-	// Save entries to database
-	
-	var processedTrackPoints = [];
+			try {		
 
-	trackPoints.forEach(function (trackPoint) {
-    	trackPoint.save(function (err) {
-     		processedTrackPoints.push(err);
+				// Verify that the submitted data is valid.
+				check(parseInt(inReachEntry.imei), 'Missing or invalid imei.').notNull().isNumeric().len(15,15);
+				check(parseInt(inReachEntry.point.latitude), 'Missing or invalid latitude.').isNumeric();
+				check(parseInt(inReachEntry.point.longitude), 'Missing or invalid longitude.').isNumeric();
+				check(parseInt(inReachEntry.point.altitude), 'Missing or invalid altitude.').isNumeric();
+				check(parseInt(inReachEntry.point.course), 'Missing or invalid course.').isNumeric();
+				check(parseInt(inReachEntry.point.speed), 'Missing or invalid speed.').isNumeric();
+				check(parseInt(inReachEntry.messageCode), 'Missing or invalid messageCode.').isNumeric();
+				check(moment(inReachEntry.timeStamp).isValid(), 'Missing or invalid timeStamp.').equals(true); 
 
-        	if (err)
-        	{
-				callback('{ "Error": "' + err.message + '"}', 400);
+				// Create a new trackPoint.
+				var trackPoint = new mongooseModels.TrackPoint({ 
+					trackerId: inReachEntry.imei,
+					messageCode: inReachEntry.messageCode,
+					message: inReachEntry.freeText,
+					timeStamp: inReachEntry.timeStamp,
+					location: { longitude: inReachEntry.point.longitude, latitude: inReachEntry.point.latitude },
+					altitude: inReachEntry.point.altitude,
+					course: inReachEntry.point.course,
+					speed: inReachEntry.point.speed
+				});    
+			} 
+
+			catch (e) {
+
+				// Invalid data submitted. Return error response.
+				sendResponse('{ "Error": "' + e.message + '"}', 400);
+				return;
 			}
 
-        	else if (processedTrackPoints.length === trackPoints.length)
-        	{
-           		callback(JSON.stringify(trackPoints), 200);
-				globalEvents.ee.emit('inReachTracksSaved');
-        	}	
-    	});	
-	});
+			// If everything went fine - push trackPoint to trackPoints array.
+			trackPoints.push(trackPoint);
+	}
+		commitInReachData(trackPoints);
+
+	}
+
+	function commitInReachData(trackPoints)
+	{
+		
+		// Create counting array.
+		var processedTrackPoints = [];
+
+		// Save entries to database
+		trackPoints.forEach(function (trackPoint) {
+    		trackPoint.save(function (err) {
+
+     			processedTrackPoints.push(err);
+
+        		if (err)
+        		{
+					// If any errors occur during save, return error response.
+					sendResponse('{ "Error": "' + err.message + '"}', 400);
+				}
+
+        		else if (processedTrackPoints.length === trackPoints.length)
+        		{
+        			// Save successful - return a receipt to the poster.
+           			sendResponse(JSON.stringify(trackPoints), 200);
+					
+					// Fire the update event to update end users.
+					globalEvents.ee.emit('inReachTracksSaved');
+        		}	
+    		});	
+		});
+
+	}
+
+	function sendResponse(message, statusCode)
+	{
+		// Return a status message to the client.
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		res.send(message, statusCode);
+		res.end;
+	}
 }
